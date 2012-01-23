ef1bbcafd6eec344048d7a0ff8f41513a310a217
Kevin O'Neill <kevin@oneill.id.au>
2012-01-23 19:07:55 +1100

    [ADD] Expose NSDictionary map:

--- UsefulBits/Foundation/NSDictionary+Blocks.h
+++ UsefulBits/Foundation/NSDictionary+Blocks.h
@@ -3,7 +3,6 @@
 //  UsefulBits
 //
 //  Created by Kevin O'Neill on 25/11/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -11,6 +10,7 @@
 @interface NSDictionary (Blocks)
 
 - (void)each:(void (^) (id key, id value))action;
+- (NSArray *)map:(id (^) (id key, id value))action;
               
 - (void)withValueForKey:(id)key meetingCondition:(BOOL (^) (id value))condition do:(void (^) (id value))action;
 - (void)withValueForKey:(id)key ofClass:(Class)class do:(void (^) (id value))action;
